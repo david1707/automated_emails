ab85372bbacbf1195b2fad7fe2cc5bbe6e8710a5
david1707 <DavidAlacant@gmail.com>
2019-09-20 21:31:19 +0200

    Sending lists to bother more than one people in a row

--- email_server.py
+++ email_server.py
@@ -1,4 +1,5 @@
-import smtplib, ssl
+import smtplib
+import ssl
 from email.mime.text import MIMEText
 from email.utils import formataddr
 
@@ -7,9 +8,11 @@ sender_email = 'smtpforletslearnabout@gmail.com'
 sender_name = 'David from LetsLearnAbout.net'
 password = input('Please, type your password:\n')
 
-receiver_email = 'smtpforletslearnabout@gmail.com'
-receiver_name = 'My Alter-ego'
-
+# receiver_email = 'smtpforletslearnabout@gmail.com'
+# receiver_name = 'My Alter-ego'
+receiver_emails = ['smtpforletslearnabout@gmail.com',
+                   'smtpforletslearnabout@gmail.com', 'smtpforletslearnabout@gmail.com']
+receiver_names = ['My Alter-ego', 'Karen from the gym', 'Kevin from the subway']
 
 # Email text
 email_body = '''
@@ -18,29 +21,31 @@ email_body = '''
 
 
 def send_email(sender_email, sender_name, password, receiver_email, receiver_name):
-		print("Sending the email...")
+    print(f'Sending an email to {receiver_name}...')
 
-		# Configurating user's info
-		msg = MIMEText(email_body, 'plain')
-		msg['To'] = formataddr((receiver_name, receiver_email))
-		msg['From'] = formataddr((sender_name, sender_email))
-		msg['Subject'] = 'Hello, my friend ' + receiver_name
+    # Configurating user's info
+    msg = MIMEText(email_body, 'plain')
+    msg['To'] = formataddr((receiver_name, receiver_email))
+    msg['From'] = formataddr((sender_name, sender_email))
+    msg['Subject'] = 'Hello, my friend ' + receiver_name
 
-		try:
-				# 587 with TLS, 465 SSL and 25
-				server = smtplib.SMTP('smtp.gmail.com', 587)
-				context = ssl.create_default_context()
-				server.starttls(context=context)
-				server.login(sender_email, password)
-				server.sendmail(sender_email, receiver_email, msg.as_string())
+    try:
+        # 587 with TLS, 465 SSL and 25
+        server = smtplib.SMTP('smtp.gmail.com', 587)
+        context = ssl.create_default_context()
+        server.starttls(context=context)
+        server.login(sender_email, password)
+        server.sendmail(sender_email, receiver_email, msg.as_string())
 
-				print('Email sent!')
-		except Exception as e:
-				print(f'Oh no! Something bad happened!\n {e}')
-		finally:
-				print('Closing the server...')
-				server.quit()
+        print('Email sent!')
+    except Exception as e:
+        print(f'Oh no! Something bad happened!\n {e}')
+    finally:
+        print('Closing the server...')
+        server.quit()
 
 
 if __name__ == "__main__":
-		send_email(sender_email, sender_name, password, receiver_email, receiver_name)
+    for receiver_email, receiver_name in zip(receiver_emails, receiver_names):
+        send_email(sender_email, sender_name, password,
+                   receiver_email, receiver_name)
