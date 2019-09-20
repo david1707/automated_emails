310db34a441f58f9904889bf1cd3a21f9006aa19
david1707 <DavidAlacant@gmail.com>
2019-09-20 22:32:07 +0200

    Sending files within emails :-)

--- email_server.py
+++ email_server.py
@@ -1,7 +1,10 @@
 import smtplib
 import ssl
 from email.mime.text import MIMEText
+from email.mime.multipart import MIMEMultipart
+from email.mime.base import MIMEBase
 from email.utils import formataddr
+from email import encoders
 
 # User configuration
 sender_email = 'smtpforletslearnabout@gmail.com'
@@ -12,9 +15,9 @@ password = 'nonono33'
 # receiver_email = 'smtpforletslearnabout@gmail.com'
 # receiver_name = 'My Alter-ego'
 receiver_emails = ['smtpforletslearnabout@gmail.com',
-                   'smtpforletslearnabout@gmail.com', 'smtpforletslearnabout@gmail.com']
+				   'smtpforletslearnabout@gmail.com', 'smtpforletslearnabout@gmail.com']
 receiver_names = ['My Alter-ego',
-                  'Karen from the gym', 'Kevin from the subway']
+				  'Karen from the gym', 'Kevin from the subway']
 
 # Email text
 # email_body = '''
@@ -23,27 +26,49 @@ receiver_names = ['My Alter-ego',
 email_html = open('email.html')
 email_body = email_html.read()
 
+filename = 'cat.gif'
+
+
 for receiver_email, receiver_name in zip(receiver_emails[:1], receiver_names[:1]):
 
-    print(f'Sending an email to {receiver_name}...')
+	print(f'Sending an email to {receiver_name}...')
 
-    # Configurating user's info
-    msg = MIMEText(email_body, 'html')
-    msg['To'] = formataddr((receiver_name, receiver_email))
-    msg['From'] = formataddr((sender_name, sender_email))
-    msg['Subject'] = 'Hello, my friend ' + receiver_name
+	# Configurating user's info
+	msg = MIMEMultipart()
+	msg['To'] = formataddr((receiver_name, receiver_email))
+	msg['From'] = formataddr((sender_name, sender_email))
+	msg['Subject'] = 'Hello, my friend ' + receiver_name
 
-    try:
-        # 587 with TLS, 465 SSL and 25
-        server = smtplib.SMTP('smtp.gmail.com', 587)
-        context = ssl.create_default_context()
-        server.starttls(context=context)
-        server.login(sender_email, password)
-        server.sendmail(sender_email, receiver_email, msg.as_string())
+	msg.attach(MIMEText(email_body, 'html'))
 
-        print('Email sent!')
-    except Exception as e:
-        print(f'Oh no! Something bad happened!\n {e}')
-    finally:
-        print('Closing the server...')
-        server.quit()
+	# Open PDF file in binary mode
+	with open(filename, "rb") as attachment:
+		# Add file as application/octet-stream
+		# Email client can usually download this automatically as attachment
+		part = MIMEBase("application", "octet-stream")
+		part.set_payload(attachment.read())
+
+	# Encode file in ASCII characters to send by email    
+	encoders.encode_base64(part)
+
+	# Add header as key/value pair to attachment part
+	part.add_header(
+		"Content-Disposition",
+		f"attachment; filename= {filename}",
+	)
+	msg.attach(part)
+
+	try:
+		# 587 with TLS, 465 SSL and 25
+		server = smtplib.SMTP('smtp.gmail.com', 587)
+		context = ssl.create_default_context()
+		server.starttls(context=context)
+		server.login(sender_email, password)
+		server.sendmail(sender_email, receiver_email, msg.as_string())
+
+		print('Email sent!')
+	except Exception as e:
+		print(f'Oh no! Something bad happened!\n {e}')
+	finally:
+		print('Closing the server...')
+		server.quit()
