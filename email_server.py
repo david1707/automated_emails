3355ecfabeb326d578857c47741a3a09c2f0b5f8
david1707 <DavidAlacant@gmail.com>
2019-09-20 21:11:17 +0200

    First version of the script. It just sends a plain text email. No headers

--- email_server.py
+++ email_server.py
@@ -1,11 +1,5 @@
-import smtplib, ssl
-
-
-"""
-  Simple email sender
-
-  No configuration, no MIMEText, plain text email
-"""
+import smtplib
+import ssl
 
 
 # User configuration
@@ -15,27 +9,27 @@ password = input('Please, type your password:\n')
 
 # Email text
 email_body = '''
-  This is a test email sent by Python. Isn't that cool?
+	This is a test email sent by Python. Isn't that cool?
 '''
 
 
-def send_email():
-	print("Sending the email...")
-    try:
-        # 587 with TLS, 465 SSL and 25
-        server = smtplib.SMTP('smtp.gmail.com', 587)
-        context = ssl.create_default_context()
-        server.starttls(context=context)
-        server.login(sender_email, password)
-        server.sendmail(sender_email, receiver_email, email_body)
+def send_email(sender, password, receiver):
+		print("Sending the email...")
+		try:
+				# 587 with TLS, 465 SSL and 25
+				server = smtplib.SMTP('smtp.gmail.com', 587)
+				context = ssl.create_default_context()
+				server.starttls(context=context)
+				server.login(sender, password)
+				server.sendmail(sender, receiver, email_body)
 
-        print('Email sent!')
-    except Exepction as e:
-        print(f'Oh no! Something bad happened!\n {e}')
-    finally:
-        print('Closing the server...')
-        server.quit()
+				print('Email sent!')
+		except Exepction as e:
+				print(f'Oh no! Something bad happened!\n {e}')
+		finally:
+				print('Closing the server...')
+				server.quit()
 
 
 if __name__ == "__main__":
-    send_email()
+		send_email(sender_email, password, receiver_email)
