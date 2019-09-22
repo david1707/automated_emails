86092e7e3de068c9234280cf784b7519905d71a8
david1707 <DavidAlacant@gmail.com>
2019-09-22 13:10:08 +0200

    Final c- bah. Probably not final

--- email_server.py
+++ email_server.py
@@ -12,9 +12,9 @@ sender_name = 'David from LetsLearnAbout.net'
 password = input('Please, type your password:\n')
 
 receiver_emails = ['smtpforletslearnabout@gmail.com',
-                   'smtpforletslearnabout@gmail.com', 'smtpforletslearnabout@gmail.com']
+									 'smtpforletslearnabout@gmail.com', 'smtpforletslearnabout@gmail.com']
 receiver_names = ['My Alter-ego',
-                  'Karen from the gym', 'Kevin from the subway']
+									'Karen from the gym', 'Kevin from the subway']
 
 # Email body
 email_html = open('email.html')
@@ -23,44 +23,49 @@ email_body = email_html.read()
 filename = 'cat.gif'
 
 for receiver_email, receiver_name in zip(receiver_emails, receiver_names):
-    print("Sending the email...")
-    # Configurating user's info
-    msg = MIMEMultipart()
-    msg['To'] = formataddr((receiver_name, receiver_email))
-    msg['From'] = formataddr((sender_name, sender_email))
-    msg['Subject'] = 'Hello, my friend ' + receiver_name
-    msg.attach(MIMEText(email_body, 'html'))
+		print("Sending the email...")
+		# Configurating user's info
+		msg = MIMEMultipart()
+		msg['To'] = formataddr((receiver_name, receiver_email))
+		msg['From'] = formataddr((sender_name, sender_email))
+		msg['Subject'] = 'Hello, my friend ' + receiver_name
+		
+		msg.attach(MIMEText(email_body, 'html'))
 
-    # Open PDF file in binary mode
-    with open(filename, "rb") as attachment:
-            part = MIMEBase("application", "octet-stream")
-            part.set_payload(attachment.read())
+		try:
+			# Open PDF file in binary mode
+			with open(filename, "rb") as attachment:
+							part = MIMEBase("application", "octet-stream")
+							part.set_payload(attachment.read())
 
-        # Encode file in ASCII characters to send by email
-    encoders.encode_base64(part)
+			# Encode file in ASCII characters to send by email
+			encoders.encode_base64(part)
 
-    # Add header as key/value pair to attachment part
-    part.add_header(
-        "Content-Disposition",
-        f"attachment; filename= {filename}",
-    )
+			# Add header as key/value pair to attachment part
+			part.add_header(
+					"Content-Disposition",
+					f"attachment; filename= {filename}",
+			)
 
-    msg.attach(part)
+			msg.attach(part)
+		except Exception as e:
+				print(f'Oh no! We didn\'t found the attachment!\n{e}')
+				break
 
-    try:
-        # Creating a SMTP session | use 587 with TLS, 465 SSL and 25
-        server = smtplib.SMTP('smtp.gmail.com', 587)
-        # Encrypts the email
-        context = ssl.create_default_context()
-        server.starttls(context=context)
-        # We log in into our Google account
-        server.login(sender_email, password)
-        # Sending email from sender, to receiver with the email body
-        server.sendmail(sender_email, receiver_email, msg.as_string())
-        print('Email sent!')
-    except Exception as e:
-        print(f'Oh no! Something bad happened!\n {e}')
-        break
-    finally:
-        print('Closing the server...')
-        server.quit()
+		try:
+				# Creating a SMTP session | use 587 with TLS, 465 SSL and 25
+				server = smtplib.SMTP('smtp.gmail.com', 587)
+				# Encrypts the email
+				context = ssl.create_default_context()
+				server.starttls(context=context)
+				# We log in into our Google account
+				server.login(sender_email, password)
+				# Sending email from sender, to receiver with the email body
+				server.sendmail(sender_email, receiver_email, msg.as_string())
+				print('Email sent!')
+		except Exception as e:
+				print(f'Oh no! Something bad happened!\n{e}')
+				break
+		finally:
+				print('Closing the server...')
+				server.quit()
